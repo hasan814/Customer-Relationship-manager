702f8498f9f5120867842527435e22e0865c8771
hasan814 <h.mousavi910@gmail.com>
2024-06-02 15:14:32 +0330

    Customer API

---
+++ src/app/api/customer/route.js
@@ -0,0 +1,36 @@
+import Customer from "@/models/Customer";
+import connectDB from "@/utils/connectDB";
+import { NextResponse } from "next/server";
+
+export const POST = async (req) => {
+  try {
+    await connectDB();
+
+    // Parse the request body
+    const body = await req.json();
+
+    const { data } = body || {};
+
+    if (!data || !data.name || !data.lastName || !data.email) {
+      return NextResponse.json(
+        { status: "failed", message: "Invalid Data" },
+        { status: 400 }
+      );
+    }
+
+    // Create a new customer
+    const customer = await Customer.create(data);
+
+    // Send success response
+    return NextResponse.json(
+      { status: "success", message: "Data created", data: customer },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { status: "failed", message: "Error in Storing Data in DB" },
+      { status: 500 }
+    );
+  }
+};
